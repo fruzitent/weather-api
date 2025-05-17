20866aeef9a538c9a23c25c62d016f511a624130
fruzitent <fruzit@fruzit.pp.ua>
2025-05-17 11:27:29 +0300

    feat(weatherapi): add weatherapi

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,8 +5,9 @@ import (
 )
 
 type Config struct {
-	Http   Http
-	Sqlite Sqlite
+	Http       Http
+	Sqlite     Sqlite
+	WeatherApi WeatherApi
 }
 
 type Http struct {
@@ -18,12 +19,17 @@ type Sqlite struct {
 	DataSourceName string
 }
 
+type WeatherApi struct {
+	SecretFile string
+}
+
 func NewConfig() *Config {
 	config := &Config{}
 
 	flag.StringVar(&config.Http.Host, "http.host", "[::]", "")
 	flag.IntVar(&config.Http.Port, "http.port", 8000, "")
 	flag.StringVar(&config.Sqlite.DataSourceName, "sqlite.dataSourceName", "db.sqlite3", "")
+	flag.StringVar(&config.WeatherApi.SecretFile, "weatherApi.secretFile", "weatherapi-token", "")
 	flag.Parse()
 
 	return config
--- readme.md
+++ readme.md
@@ -2,3 +2,10 @@
 
 > [!NOTE]
 > Deployment is maintained externally at [fruzitent/infra](https://git.fruzit.pp.ua/fruzitent/infra/compare/main...ses/5.0)
+
+## Quick Start
+
+```shell
+gum input --password | install -D "/dev/stdin" "./weatherapi-token"
+weather-api daemon
+```
